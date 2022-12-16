67f16d978d7173d46747ccc6d99fa455d388a2da
Steve43210 <StevenJasonDorsett@gmail.com>
2022-12-16 10:50:00 -0500

    Add files via upload

---
+++ calculator/resources/js/script.js
@@ -0,0 +1,54 @@
+document.getElementById("num1").onclick = function write1()
+{
+document.getElementById("screen").innerHTML = "1";
+}
+
+document.getElementById("num2").onclick = function write1()
+{
+document.getElementById("screen").innerHTML = "2";
+}
+
+document.getElementById("num3").onclick = function write1()
+{
+document.getElementById("screen").innerHTML = "3";
+}
+
+document.getElementById("num4").onclick = function write1()
+{
+document.getElementById("screen").innerHTML = "4";
+}
+
+document.getElementById("num5").onclick = function write1()
+{
+document.getElementById("screen").innerHTML = "5";
+}
+
+document.getElementById("num6").onclick = function write1()
+{
+document.getElementById("screen").innerHTML = "6";
+}
+
+document.getElementById("num7").onclick = function write1()
+{
+document.getElementById("screen").innerHTML = "7";
+}
+
+document.getElementById("num8").onclick = function write1()
+{
+document.getElementById("screen").innerHTML = "8";
+}
+
+document.getElementById("num9").onclick = function write1()
+{
+document.getElementById("screen").innerHTML = "9";
+}
+
+document.getElementById("num0").onclick = function write1()
+{
+document.getElementById("screen").innerHTML = "0";
+}
+
+document.getElementById("clear").onclick = function write1()
+{
+document.getElementById("screen").innerHTML = "";
+}
