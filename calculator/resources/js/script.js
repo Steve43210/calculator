622d1344487b82efba2c9da1105b99381558ebb5
Steve43210 <StevenJasonDorsett@gmail.com>
2022-12-19 12:53:16 -0500

    numbers

--- calculator/resources/js/script.js
+++ calculator/resources/js/script.js
@@ -1,54 +1,81 @@
+var $screen = $("#screen");
+
 document.getElementById("num1").onclick = function write1()
 {
-document.getElementById("screen").innerHTML = "1";
+ $screen.append("1")
 }
 
 document.getElementById("num2").onclick = function write1()
 {
-document.getElementById("screen").innerHTML = "2";
+ $screen.append("2")
 }
 
 document.getElementById("num3").onclick = function write1()
 {
-document.getElementById("screen").innerHTML = "3";
+ $screen.append("3")
 }
 
 document.getElementById("num4").onclick = function write1()
 {
-document.getElementById("screen").innerHTML = "4";
+$screen.append("4")
 }
 
 document.getElementById("num5").onclick = function write1()
 {
-document.getElementById("screen").innerHTML = "5";
+$screen.append("5")
 }
 
 document.getElementById("num6").onclick = function write1()
 {
-document.getElementById("screen").innerHTML = "6";
+$screen.append("6")
 }
 
 document.getElementById("num7").onclick = function write1()
 {
-document.getElementById("screen").innerHTML = "7";
+$("#screen").append("7")
 }
 
 document.getElementById("num8").onclick = function write1()
 {
-document.getElementById("screen").innerHTML = "8";
+$("#screen").append("8")
 }
 
 document.getElementById("num9").onclick = function write1()
 {
-document.getElementById("screen").innerHTML = "9";
+$("#screen").append("9")
 }
 
 document.getElementById("num0").onclick = function write1()
 {
-document.getElementById("screen").innerHTML = "0";
+$("#screen").append("0")
 }
 
 document.getElementById("clear").onclick = function write1()
 {
-document.getElementById("screen").innerHTML = "";
+$("#screen").empty()
+}
+
+document.getElementById("pluse").onclick = function write1()
+{
+$("#screen").append("+")
+}
+
+document.getElementById("minus").onclick = function write1()
+{
+$("#screen").append("-")
+}
+
+document.getElementById("multiply").onclick = function write1()
+{
+$("#screen").append("x")
+}
+
+document.getElementById("divide").onclick = function write1()
+{
+$("#screen").append("/")
+}
+
+document.getElementById("equal").onclick = function write1()
+{
+$("#screen").append("=")
 }
