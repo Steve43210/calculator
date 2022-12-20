b12ed6d0455320537f2dd73bf3c61fe34af143d6
Steve43210 <StevenJasonDorsett@gmail.com>
2022-12-20 12:29:32 -0500

    numbers

--- calculator/resources/js/script.js
+++ calculator/resources/js/script.js
@@ -1,54 +1,18 @@
 var $screen = $("#screen");
+var $number = $(".number");
 
-document.getElementById("num1").onclick = function write1()
-{
- $screen.append("1")
+$number.on('click', function() {
+var numberPressed = $(this).html();
+$screen.append(numberPressed);
+
+if (num1 == 11) {
+ num1 = numberPressed;
+}else if (num2 == 11) {
+ num2 = numberPressed;
 }
 
-document.getElementById("num2").onclick = function write1()
-{
- $screen.append("2")
-}
+});
 
-document.getElementById("num3").onclick = function write1()
-{
- $screen.append("3")
-}
-
-document.getElementById("num4").onclick = function write1()
-{
-$screen.append("4")
-}
-
-document.getElementById("num5").onclick = function write1()
-{
-$screen.append("5")
-}
-
-document.getElementById("num6").onclick = function write1()
-{
-$screen.append("6")
-}
-
-document.getElementById("num7").onclick = function write1()
-{
-$("#screen").append("7")
-}
-
-document.getElementById("num8").onclick = function write1()
-{
-$("#screen").append("8")
-}
-
-document.getElementById("num9").onclick = function write1()
-{
-$("#screen").append("9")
-}
-
-document.getElementById("num0").onclick = function write1()
-{
-$("#screen").append("0")
-}
 
 document.getElementById("clear").onclick = function write1()
 {
@@ -58,24 +22,29 @@ $("#screen").empty()
 document.getElementById("pluse").onclick = function write1()
 {
 $("#screen").append("+")
+op = "+";
 }
 
 document.getElementById("minus").onclick = function write1()
 {
 $("#screen").append("-")
+op = "-";
 }
 
 document.getElementById("multiply").onclick = function write1()
 {
 $("#screen").append("x")
+op = "x";
 }
 
 document.getElementById("divide").onclick = function write1()
 {
 $("#screen").append("/")
+op = "/";
 }
 
 document.getElementById("equal").onclick = function write1()
 {
 $("#screen").append("=")
+op = "=";
 }
